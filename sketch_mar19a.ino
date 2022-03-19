f45c89465f9344b0620e59f14a5a572bf7e16201
Ishan Narvekar <71626293+narvekar77@users.noreply.github.com>
2022-03-19 19:05:11 +0530

    Add files via upload
    new update ..

---
+++ sketch_mar19a.ino
@@ -0,0 +1,123 @@
+//Face Tracker using OpenCV and Arduino
+//neck
+
+#include<Servo.h>
+#define ml 4
+#define mr 5
+Servo y, y1, y2;//y1 
+
+int x;
+int width = 640, height = 480;  // total resolution of the video
+int xpos = 90, ypos = 10, y1pos = 20, y2pos;
+int motorspeed = 0;
+int xspeed = 0;// initial positions of both Servos
+void setup() {
+//
+  Serial.begin(115200);
+  pinMode(ml, OUTPUT);
+  pinMode(mr, OUTPUT);
+  y.attach(3);
+  y1.attach(6);
+  y2.attach(9);
+  // Serial.print(width);
+  //Serial.print("\t");
+  //Serial.println(height);  
+  y.write(ypos);
+  y1.write(y1pos);
+  y2.write(y2pos);
+}
+const int angle = 2;   // degree of increment or decrement
+
+void loop() {
+  if (Serial.available() > 0)
+  {
+    int x_mid, y_mid, y1_mid, y2_mid;
+    if (Serial.read() == 'X')
+    {
+      x_mid = Serial.parseInt();  // read center x-coordinate
+      if (Serial.read() == 'Y')
+        y_mid = Serial.parseInt();
+        y1_mid = y1_mid;//Serial.parseInt();//y_mid;
+        y2_mid = y_mid;// read center y-coordinate
+    }
+    /* adjust the servo within the squared region if the coordinates
+        is outside it
+    */
+    if ((x_mid > width / 2 + 30)&&(x_mid > 0 && x_mid < width))
+      xpos += angle;
+     if ((x_mid < width / 2 - 30)&&(x_mid > 0 && x_mid < width))
+      xpos -= angle;   
+    if (y_mid < height / 2 + 30)
+      {
+        ypos -= angle;
+        y1pos += angle;//-
+        y2pos -= angle;
+      }
+      
+    if (y_mid > height / 2 - 30)
+      {
+        ypos += angle;
+        y1pos -= angle;//+
+        y2pos += angle;
+      }
+
+
+       if (x_mid > width / 2 + 30) {
+        digitalWrite(ml, LOW);//H
+        digitalWrite(mr, HIGH);//L
+        delay(7);
+        digitalWrite(ml, LOW);
+        digitalWrite(mr, LOW);
+        motorspeed = map(xpos, 320, 0, 0, 255);
+      }
+      else if(x_mid < width / 2 - 30) {
+        digitalWrite(ml, HIGH);//L
+        digitalWrite(mr, LOW);//H
+        delay(7);
+        digitalWrite(ml, LOW);
+        digitalWrite(mr, LOW);
+        motorspeed = map(xpos, 320, 1023, 0, 255);
+      }
+      else {
+        motorspeed = 0;
+        digitalWrite(ml, LOW);
+        digitalWrite(mr, LOW);
+      }
+      if (motorspeed < 70) {
+          motorspeed = 0;
+      }
+
+    // if the servo degree is outside its range//error handling code 
+    if (xpos >= 180)//180
+      xpos = 180;
+    else if (xpos <= 0)
+      xpos = 0;
+    if (ypos >= 40)
+      ypos = 40;
+    else if (ypos <= 0)
+      ypos = 0;
+    if (y1pos  >= 20)//40
+      y1pos = 20;
+    else if (y1pos <= 0)
+      y1pos = 0;
+    if (y2pos  >= 40)
+      y2pos = 40;
+    else if (y2pos <= 0)
+      y2pos = 0;
+
+  
+    y.write(ypos);
+    y1.write(y1pos);//y
+    y2.write(ypos);//y
+  }
+  else {
+    digitalWrite(ml, LOW);
+    digitalWrite(mr, LOW); 
+ }
+    // used for testing
+    //Serial.print("\t");
+    // Serial.print(x_mid);
+    // Serial.print("\t");
+    // Serial.println(y_mid);
+  
+}
